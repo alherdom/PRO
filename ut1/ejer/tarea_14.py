3dc81da4006ee54b9629198fded50f529e40ee6d
alherdom <alejandrohd1@live.com>
2022-10-20 13:26:44 +0100

    Prueba

--- ut1/ejer/tarea_14.py
+++ ut1/ejer/tarea_14.py
@@ -1,12 +1,12 @@
 # Escriba un programa en Python que acepte la opción de dos jugadoras en Piedra-Papel-Tijera y decida el resultado (solución).
 # Entrada: persona1=piedra; persona2=papel
 # Salida: Gana persona2: El papel envuelve a la piedra
-piedra = input('Escoge piedra?[s/n]') == 's'
-papel = input('Escoge papel?[s/n]') == 's'
-tijera = input('Escoge tijera?[s/n]') == 's'
-if piedra and papel:
-    print('Gana papel')
-elif tijera and not piedra:
-    print('Gana tijera')
-if not papel and piedra:
-    print('Gana piedra')
+persona1 = input('Piedra, papel o tijera, saca lo que quieras ya!') 
+persona2 = input('Piedra, papel o tijera, saca lo que quieras ya!')
+
+if persona1 == 'piedra' and persona2 == 'papel':
+    print('Gana persona2: El papel envuelve a la piedra')
+elif persona1 == 'tijera' and not persona2 == 'piedra':
+    print('Gana persona1: La tijera corta el papel')
+if not persona1 == 'papel' and persona2 == 'piedra':
+    print('Gana persona2: La piedra rompe las tijeras')
