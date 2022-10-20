3aa6402d06ae5990f4896f7c16c21deee56a9b0d
alherdom <alejandrohd1@live.com>
2022-10-20 13:29:52 +0100

    Prueba

--- ut1/ejer/tarea_14.py
+++ ut1/ejer/tarea_14.py
@@ -1,12 +1,33 @@
 # Escriba un programa en Python que acepte la opción de dos jugadoras en Piedra-Papel-Tijera y decida el resultado (solución).
 # Entrada: persona1=piedra; persona2=papel
 # Salida: Gana persona2: El papel envuelve a la piedra
-persona1 = input('Piedra, papel o tijera, saca lo que quieras ya!') 
-persona2 = input('Piedra, papel o tijera, saca lo que quieras ya!')
-
-if persona1 == 'piedra' and persona2 == 'papel':
-    print('Gana persona2: El papel envuelve a la piedra')
-elif persona1 == 'tijera' and not persona2 == 'piedra':
-    print('Gana persona1: La tijera corta el papel')
-if not persona1 == 'papel' and persona2 == 'piedra':
-    print('Gana persona2: La piedra rompe las tijeras')
+persona1 = input('Piedra, papel o tijera, saca lo que quieras ya! ')
+persona2 = input('Piedra, papel o tijera, saca lo que quieras ya! ')
+eleccion1 = persona1.lower()
+eleccion2 = persona2.lower()
+print('La persona1 ha elegido: ',eleccion1)
+print('La persona2 ha elegido: ',eleccion2)
+if eleccion1 == 'piedra' and eleccion2 == 'papel':
+    ganador = 2
+    mensaje ='EL PAPEL ENVUELVE A LA PIEDRA'
+if eleccion1 == 'papel' and eleccion2 == 'piedra':
+    ganador = 1
+    mensaje ='EL PAPEL ENVUELVE A LA PIEDRA'
+if eleccion1 == 'piedra' and persona2 == 'tijera':
+    ganador = 1
+    mensaje ='LA PIEDRA ROMPE LA LA TIJERA'
+if eleccion1 == 'tijera' and eleccion2 == 'piedra':
+    ganador = 2
+    mensaje ='LA PIEDRA ROMPE LA LA TIJERA'
+if eleccion1 == 'tijera' and eleccion2 == 'papel':
+    ganador = 1
+    mensaje ='LA TIJERA CORTA EL PAPEL'
+if eleccion1 == 'papel' and eleccion2 == 'tijera':
+    ganador = 2
+    mensaje ='LA TIJERA CORTA EL PAPEL'
+if eleccion1 == eleccion2:
+    mensaje = 'EMPATE'
+    ganador = 0
+    print("EMPATE")
+else:
+    print(f'Gana persona{ganador}: {mensaje}')
