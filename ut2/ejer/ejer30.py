5b04205ff25f3350db603e5db591a1139ac2f15b
Alejandro <alejandrohd1@live.com>
2022-10-26 19:02:20 +0100

    Sinc

---
+++ ut2/ejer/ejer30.py
@@ -0,0 +1,4 @@
+# Escriba un programa que calcule el máximo común divisor entre dos números enteros.
+# No utilice ningún algoritmo existente. Hágalo probando divisores.
+# Entrada: a=12; b=44
+# Salida: 4
