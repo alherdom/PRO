c25ad91f642bfaeb01236a67ace344c89c19bf46
alherdom <alejandrohd1@live.com>
2022-11-08 11:50:30 +0000

    Sinc

--- ut2/ejer/condicional/calcfuel.py
+++ ut2/ejer/condicional/calcfuel.py
@@ -1,13 +1,30 @@
-# In this kata you will have to write a function that takes litres and price_per_litre (in dollar) as arguments.
-# Purchases of 2 or more litres get a discount of 5 cents per litre, purchases of 4 or more litres get a discount
-# of 10 cents per litre, and so on every two litres, up to a maximum discount of 25 cents per litre. But total
-# discount per litre cannot be more than 25 cents. Return the total cost rounded to 2 decimal places. Also you can
-# guess that there will not be negative or non-numeric inputs.
+# En esta kata tendrás que escribir una función que tome litros y precio_por_litro (en dólares) como argumentos.
+# Las compras de 2 o más litros tienen un descuento de 5 céntimos por litro, las compras de 4 o más litros tienen un descuento
+# de 10 céntimos por litro, y así cada dos litros, hasta un descuento máximo de 25 céntimos por litro. Pero el total
+# descuento por litro no puede ser superior a 25 céntimos. Devuelve el coste total redondeado a 2 decimales. También se puede
+# suponer que no habrá entradas negativas o no numéricas.
 
-litros = int(input("INTRODUZCA LOS LITROS A REPOSTAR: "))
+
+litros = float(input("INTRODUZCA LOS LITROS A REPOSTAR: "))
 precio = float(input("INTRODUZCA EL PRECIO DE LA GASOLINA BASE: "))
 
-if 2 < litros < 4:
-    descuento = 1 - (0.05 * litros)
-    precio_total = litros * precio * descuento
+if 2 <= litros < 4:
+    descuento = 0.05 * litros
+    precio_total = round(litros * precio - descuento, 2)
+    print(precio_total)
+elif 4 <= litros < 6:
+    descuento = 0.10 * litros
+    precio_total = round(litros * precio - descuento, 2)
+    print(precio_total)
+elif 6 <= litros < 8:
+    descuento = 0.15 * litros
+    precio_total = round(litros * precio - descuento, 2)
+    print(precio_total)
+elif 8 <= litros < 10:
+    descuento = 0.20 * litros
+    precio_total = round(litros * precio - descuento, 2)
+    print(precio_total)
+elif litros >= 10:
+    descuento = 0.25 * litros
+    precio_total = round(litros * precio - descuento, 2)
     print(precio_total)
