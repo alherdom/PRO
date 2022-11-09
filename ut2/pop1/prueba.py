823faadee4e8f5977ec4b13ef2c3f94a3404eb9b
alherdom <alejandrohd1@live.com>
2022-11-09 18:08:16 +0000

    Sinc

--- ut2/pop1/prueba.py
+++ ut2/pop1/prueba.py
@@ -1,13 +1,8 @@
 daughter_age = 35
 mother_age = 15
-target_mother_age = 0
-target_daughter_age = 0
 
+while mother_age != daughter_age * 2:
+    mother_age += 1
+    daughter_age += 1
 
-for i in range(daughter_age, mother_age):
-        mother_age += 1
-        daughter_age += 1
-        if mother_age == daughter_age * 2:
-            target_mother_age = mother_age
-            target_daughter_age = daughter_age
-        print(target_mother_age, target_daughter_age)
+        
