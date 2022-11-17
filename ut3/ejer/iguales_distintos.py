4507254241315175209e8df8c802a674e0775ac2
alherdom <alejandrohd1@live.com>
2022-11-17 21:49:09 +0000

    Sinc

--- ut3/ejer/iguales_distintos.py
+++ ut3/ejer/iguales_distintos.py
@@ -2,16 +2,16 @@
 # Entrada: [1, 1, 1, 1, 1, 1, 1]
 # Salida: Iguales
 
-values = [3, 3, 3, 3, 3, 3, 3]
+values = [1, 1, 1, 1, 1, 1, 1]
 first_value = values[0]
 output = []
 for value in values:
     if value == first_value:
         first_value = value
         output.append(first_value)
+    else:
+        print("ALGUNO DE LOS VALORES DE LA LISTA SON DISTINTOS")
+        print(values)
 if values == output:
     print("TODOS LOS VALORES DE LA LISTA SON IGUALES")
     print(output)
-else:
-    print("ALGUNO DE LOS VALORES DE LA LISTA SON DISTINTOS")
-    print(values)
