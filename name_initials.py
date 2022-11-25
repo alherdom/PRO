eff104f1ab67c69f83319beed78a979d31583bc8
Alejandro <alejandrohd1@live.com>
2022-11-25 16:46:09 +0000

    Sinc

--- name_initials.py
+++ name_initials.py
@@ -6,17 +6,11 @@ persona pasadas a mayúsculas y con punto al final.
 
 def run(fullname: str) -> str:
     initials = []
-    fullname = fullname.title()
-    fullname = fullname.split(', ')
-    initial_name = fullname[1][0]
-    fullname =' '.join(fullname)
-    fullname = fullname.split(' ')
-    first_lastname = fullname[0][0]
-    second_lastname = fullname[1][0]
-    initials = initial_name + first_lastname + second_lastname
-    initials ='.'.join(initials)
-
-    
+    fullname = fullname.title().split(", ")
+    name_init = fullname[1][0]
+    surname1_init = fullname[0][0]
+    surname2_init = fullname[0][1]
+    initials = print(f'{name_init}{surname1_init}.{surname2_init}')
     return initials
 
 
