01935976715d8d9f954113a6a48a4b08c7ac8123
alherdom <alejandrohd1@live.com>
2022-11-27 17:57:13 +0000

    Sync

---
+++ ut3/pop0/merge_sorted.py
@@ -0,0 +1,17 @@
+# ***************
+# MEZCLA ORDENADA
+# ***************
+
+
+def run(values1: list, values2: list) -> list:
+    merged = values1 + values2
+    output = []
+    for num in merged:
+        if num not in output:
+            output.append(num)
+    merged = output
+    return merged
+
+
+if __name__ == '__main__':
+    run([1, 2, 3, 4], [1, 1, 2, 3, 4, 5])
