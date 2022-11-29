9d1446a157100c6c63596a6889a08d693c454041
alherdom <alejandrohd1@live.com>
2022-11-29 13:00:02 +0000

    Sync

---
+++ ut3/pop0/trimmed_sum.py
@@ -0,0 +1,12 @@
+# **************
+# SUMA RECORTADA
+# **************
+
+
+def run(values: list) -> int:
+    tsum = 'tu código aquí'
+    return tsum
+
+
+if __name__ == '__main__':
+    run([6, 2, 1, 8, 10])
