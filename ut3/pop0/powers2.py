a50bc3174a41e0b88a4deb0b752875d95d5e4552
Alejandro <alejandrohd1@live.com>
2022-11-29 16:56:42 +0000

    Sync

---
+++ ut3/pop0/powers2.py
@@ -0,0 +1,15 @@
+'''
+Dado un número entero no negativo obtenga una lista con todas las potencias de 2
+con el exponente variando desde 0 hasta dicho valor (inclusive).
+
+¿Podrías resolverlo también con listas por comprensión?
+'''
+
+
+def run(num_powers: int) -> list:
+    powers2 = [num_powers**i for i in range(num_powers + 1)]
+    return powers2
+
+
+if __name__ == '__main__':
+    run(0)
