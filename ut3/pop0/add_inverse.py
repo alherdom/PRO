3b71b4bcbd8891257b972d871ce1a55a24ba414f
alherdom <alejandrohd1@live.com>
2022-11-29 13:47:58 +0000

    Sync

---
+++ ut3/pop0/add_inverse.py
@@ -0,0 +1,12 @@
+# **************
+# SUMA INVERTIDA
+# **************
+
+
+def run(numbers: list) -> int:
+    add_inv = -1 * sum(numbers)
+    return add_inv
+
+
+if __name__ == '__main__':
+    run([1, 2, 3, 4, 5])
