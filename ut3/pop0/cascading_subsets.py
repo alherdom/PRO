06c4dd722d66675e099781261b304028521bba0a
Alejandro <alejandrohd1@live.com>
2022-11-29 18:04:20 +0000

    Sync

--- ut3/pop0/cascading_subsets.py
+++ ut3/pop0/cascading_subsets.py
@@ -4,22 +4,12 @@
 
 
 def run(values: list, size: int) -> list:
+    
     cascading = []
-    if size == 3:
-        cascading.append(values[:size])
-        cascading.append(values[1:])
-    elif size == 2:
-        cascading.append(values[:size])
-        cascading.append(values[1:size+1])
-        cascading.append(values[size:])
-    elif size == 1:
-        i = 0
-        for value in values:
-            cascading.append(values[i:i+1])
-            i += 1
+    for i in range(len(values)-size +1):
+        cascading.append(values[i: i + size])
     
     return cascading
-
-
+    
 if __name__ == '__main__':
     run([1, 2, 3, 4], 3)
