f77a36c2166fd1f13a82e5fcd8e69c94d6fc4e16
alherdom <alejandroh1@live.com>
2023-01-11 16:49:30 +0000

    Sync

--- ut3/ejer/tennis_game.py
+++ ut3/ejer/tennis_game.py
@@ -11,12 +11,8 @@ def run(points: str) -> str:
             count_A += 1
         elif char == "B":
             count_B += 1
-    print("El jugador A tiene estos puntos",count_A)
-    print("El jugador B tiene estos puntos",count_B)
     if (count_A - count_B) >= 2 and count_A >=4:
-        games_A +=1
         count_A = 0
-        
         winner = "A"
     return winner
 
