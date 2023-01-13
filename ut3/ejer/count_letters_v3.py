2f38fbb935432d4240e93541a2164bbd8c112997
alherdom <alejandroh1@live.com>
2023-01-13 17:14:49 +0000

    Sync

--- ut3/ejer/count_letters_v3.py
+++ ut3/ejer/count_letters_v3.py
@@ -4,15 +4,11 @@
 
 
 def run(sentence: str) -> dict:
-    #counter = {}
-    #for char in sentence:
-    #    if char in counter:
-    #        continue
-    #    counter[char] = sentence.count(char)
-    #return counter
     counter = {}
     for char in sentence:
         counter[char] = counter.get(char,0) +1
     return counter
+
+
 if __name__ == '__main__':
     run('boom')
