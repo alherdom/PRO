f991f3872a1eec2f88f31c046611d2dea20a94bb
alherdom <alejandrohd1@live.com>
2023-01-25 00:55:34 +0000

    Sync

--- ut3/ejer/tupleset.py
+++ ut3/ejer/tupleset.py
@@ -4,14 +4,17 @@
 
 
 def run(input: tuple) -> set:
+    values1 = []
+    values2 = []
+    output = set()
     for item in input:
-        value1 = item[0][0]
-        value2 = item[0][1]
-        print(value1, value2)
-    output = set(input)
+        values1.append(item[0])
+        values2.append(item[1])
+    values1 = tuple(values1)
+    values2 = tuple(values2)
+    output = (set(values1),set(values2))
     return output
 
 
 if __name__ == '__main__':
     run(((4, 3), (8, 2), (7, 5), (8, 2), (9, 1)))
-
