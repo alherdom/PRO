3c5a498aecdae8e76ba6009c5e4c46416826e11e
alherdom <alejandrohd1@live.com>
2023-01-27 00:32:04 +0000

    Sync

--- ut3/ejer/avg_temps.py
+++ ut3/ejer/avg_temps.py
@@ -6,15 +6,27 @@ from pathlib import Path
 
 
 def run(input_path: Path) -> bool:
+    count = total = avgte = 0
+    avg_temps = []
+    
+    #---------LECTURA FICHERO. CALCULO MEDIA----------#
+    with open('data/avg_temps/temperatures.dat') as f:
+        for line in f:
+            line = line.strip().split(",")
+            for temp in line:
+                total += int(temp)
+                count += 1
+                if count == 31:
+                    avgte = "{:.2f}".format(total / 31)
+                    avg_temps.append(avgte)
+                    count = total = 0
+    
+    #-------------------SALIDA DE FICHERO-----------------#
+    with open('data/avg_temps/avg_temps.dat', 'w') as favg:
+        for avg_temp in avg_temps:
+            favg.write(str(avg_temp) + '\n')
     output_path = 'data/avg_temps/avg_temps.dat'
     
-    with open('data/avg_temps/temperatures.dat') as ftemps:
-        for temp in ftemps:
-            print(temp)
-    
-    with open('data/avg_temps/avg_temps.dat', 'w') as favg:
-        for avg_temp in favg:
-            favg.write(avg_temp)   
         
     return filecmp.cmp(output_path, 'data/avg_temps/.expected', shallow=False)
 
