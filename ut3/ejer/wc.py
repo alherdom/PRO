b7c2d72e48b1dbf5239dfcaa54a71124068959e3
alherdom <alejandrohd1@live.com>
2023-01-27 23:23:35 +0000

    Sync

--- ut3/ejer/wc.py
+++ ut3/ejer/wc.py
@@ -6,15 +6,14 @@ from pathlib import Path
 
 def run(input_path: Path) -> tuple:
     num_lines = num_words = num_bytes = 0
-    
-    #---------LECTURA FICHERO. CALCULO DE LINEAS----------#
-    with open(input_path) as f:
+    with open(input_path, encoding="utf8") as f:
         for line in f:
             num_lines += 1
-            for items in line:
-                num_bytes +=1
-
+            num_words += len(line.strip().split(" "))
+            num_bytes += len(line.encode('utf-8'))
     return num_lines, num_words, num_bytes
+            
+
 
 if __name__ == '__main__':
     run('data/wc/lorem.txt')
