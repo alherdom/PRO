98070f7bbdddda6f52ff69cf146c69cfa8beef8e
alherdom <alejandroh1@live.com>
2023-01-30 18:01:59 +0000

    Sync

--- ut3/ejer/wc.py
+++ ut3/ejer/wc.py
@@ -6,11 +6,11 @@ from pathlib import Path
 
 def run(input_path: Path) -> tuple:
     num_lines = num_words = num_bytes = 0
-    with open(input_path, encoding="utf8") as f:
+    with open(input_path) as f: #encoding="utf8"
         for line in f:
             num_lines += 1
             num_words += len(line.strip().split(" "))
-            num_bytes += len(line.encode('utf-8'))
+            num_bytes += len(line.encode('utf-8')) 
     return num_lines, num_words, num_bytes
             
 
