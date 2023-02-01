221e2ac653948ec0a844641aef438e65fb2e587d
alherdom <alejandrohd1@live.com>
2023-02-01 23:01:05 +0000

    Sync

--- ut3/ejer/find_words.py
+++ ut3/ejer/find_words.py
@@ -4,36 +4,24 @@
 from pathlib import Path
 
 
-def run(data_path: Path, target_word: str) -> list:
-    matches = []
-    values = ()
-    num_row = num_column = 0
-    trash = ".,;:()!¿'"
-    with open(data_path, 'r') as f:
+def run(data_path: Path, target_word: str) -> list: 
+    with open(data_path, encoding="UTF-8") as f:
+        
+        TRASH = "(,)'-¡!"
+        target_word = target_word.lower()
+        matches = []
+        row = 0    
+        
         for line in f:
+            row += 1
             line = line.lower()
-            num_row += 1         
-            list_words = line.lower().strip().split()
-            target_word = target_word.lower()  
-            its_here = False
-            num_times = list_words.count(target_word)
-            if num_times == 2:
-                print(target_word)
-                num_column = line.index(target_word) + 1
-                values = (num_row,num_column)
-                matches.append(values)
-
-        #USO EL "if" Y UN BOOLEANO PARA SABER SI LA PALABRA ESTÁ ÚNICAMENTE EN LA LISTA
-            for word in list_words:
-                word = word.strip(trash)
-                if word == target_word:
-                    its_here = True
-        #USO "in" PARA SABER SI LA PALABRA ESTA EN LA LINEA        
-            if target_word in line.strip() and its_here:
-                    num_column = line.index(target_word) + 1
-                    values = (num_row,num_column)
-                    matches.append(values)
-       
+            words = line.strip().split()
+            
+            for word in words:                     
+                if word.strip(TRASH) == target_word:
+                    column = line.index(target_word) + 1
+                    matches.append((row,column))
+                    
     return matches
     
 
