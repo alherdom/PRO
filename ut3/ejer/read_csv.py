44645779bd3fed9b288554e9fa045600e77ad9c0
alherdom <alejandroh1@live.com>
2023-02-01 18:03:31 +0000

    Sync

--- ut3/ejer/read_csv.py
+++ ut3/ejer/read_csv.py
@@ -6,22 +6,21 @@ from pathlib import Path
 
 def run(datafile: Path) -> list:
     with open (datafile) as f:
-        NUMBERS="1234567890"
         data=[]
-        pokedex = {}
-        i = 0
+        firstline = f.readline()
         for lines in f:
-            key_line = line.strip().split(",")
+            pokedex = {}
+            key_line = firstline.strip().split(",")
             value_lines = lines.strip().split(",")
-            print(key_line)
-            print(value_lines)
-            for value in value_lines:
-                pokedex[key_line[i]] = value
-                i += 1
-                if i == 12:
-                    i = 0
-                    continue
-        data.append(pokedex)
+            for key, value in zip(key_line, value_lines):
+                if value.isdigit():
+                    value = int(value)
+                elif value == "False":
+                    value = False
+                elif value == "True":
+                    value = True
+                pokedex[key] = value
+            data.append(pokedex)
                     
         
      
