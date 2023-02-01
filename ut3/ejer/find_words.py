85e90fdc9f91b17b41fd8db40f4d3046eb30f86c
alherdom <alejandroh1@live.com>
2023-02-01 19:50:38 +0000

    Sync

--- ut3/ejer/find_words.py
+++ ut3/ejer/find_words.py
@@ -17,6 +17,13 @@ def run(data_path: Path, target_word: str) -> list:
             list_words = line.lower().strip().split()
             target_word = target_word.lower()  
             its_here = False
+            num_times = list_words.count(target_word)
+            if num_times == 2:
+                print(target_word)
+                num_column = line.index(target_word) + 1
+                values = (num_row,num_column)
+                matches.append(values)
+
         #USO EL "if" Y UN BOOLEANO PARA SABER SI LA PALABRA ESTÁ ÚNICAMENTE EN LA LISTA
             for word in list_words:
                 word = word.strip(trash)
