e5cb7536d33645b67bc9efcad7839978a7f7c948
alherdom <alejandrohd1@live.com>
2023-02-02 02:30:06 +0000

    Sync

--- ut3/ejer/find_words.py
+++ ut3/ejer/find_words.py
@@ -10,17 +10,23 @@ def run(data_path: Path, target_word: str) -> list:
         TRASH = "(,)'-¡!"
         target_word = target_word.lower()
         matches = []
-        row = 0    
-        
+        row = 0     
         for line in f:
             row += 1
-            line = line.lower()
-            words = line.strip().split()
-            
-            for word in words:                     
+            line = line.strip().lower()
+            words = line.split()
+            for word in words:
                 if word.strip(TRASH) == target_word:
                     column = line.index(target_word) + 1
                     matches.append((row,column))
+            if line.count(target_word) > 1:
+                column = sum([i for i, e in enumerate(words) if e == target_word]) + len(words)* 2
+                matches.append((row,column))
+        for match in matches:
+            if matches.count(match) > 1:
+                matches.remove(match)
+                    
+                
                     
     return matches
     
