4daeab56503018d7af5c407723d6651b24a133c5
alherdom <alejandroh1@live.com>
2023-02-03 20:19:46 +0000

    Sync

--- ut3/ejer/replace_chars.py
+++ ut3/ejer/replace_chars.py
@@ -7,18 +7,20 @@ from pathlib import Path
 
 def run(input_path: Path, replacements: str) -> bool: 
     with open(input_path, encoding="utf8") as finput:
+        i = 0
+        old_chars = []
+        new_chars = []
         news = finput.read()
-        if "á" in news:
-            news = news.replace("á", "a")
-        if "é" in news:
-            news = news.replace("é", "e")
-        if "í" in news:
-            news = news.replace("í", "i")
-        if "ó" in news:
-            news = news.replace("ó", "o")
-        if "ú" in news:
-            news = news.replace("ú", "u")
-    print(news)
+        replace_chars = replacements.replace("|","").split
+        old_chars.append(replace_chars[::2])
+        new_chars.append(replace_chars[1::2])
+        print(new_chars)
+        for old_char, new_char in zip(old_chars,new_chars):
+            print(old_char)
+            print(new_char)
+            news = news.replace(old_char,new_char)
+   
+        
     output_path = 'data/replace_chars/r_noticia.txt'
     with open(output_path, 'w',encoding="utf8") as foutput:
         foutput.write(news)
