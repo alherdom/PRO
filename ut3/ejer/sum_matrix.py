bf357b83d5aacb1e8c46490455aa53971aed118c
alherdom <alejandrohd1@live.com>
2023-02-03 12:08:12 +0000

    Sync

--- ut3/ejer/sum_matrix.py
+++ ut3/ejer/sum_matrix.py
@@ -6,18 +6,15 @@ from pathlib import Path
 
 
 def run(matrix1_path: Path, matrix2_path: Path) -> bool:
-    # LECTURA DE FICHEROS. METO LAS MATRICES EN DOS LISTAS:
     with open(matrix1_path) as f1, open(matrix2_path) as f2:
         matrix1 = f1.read().split()
         matrix2 = f2.read().split()
         results = []
-    # SUMO LAS DOS MATRICES MEDIANTE LA FUNCION ZIP:
         for values1, values2 in zip(matrix1, matrix2):
             results.append(int(values1)+int(values2))
-    # ESCRIBO EL RESULTADO EL FICHERO DE SALIDA:
     result_path = 'data/sum_matrix/result.dat'
     with open(result_path, 'w') as fresult:
-        count = 0         
+        count = 0
         for result in results:
             if count != 4:
                 fresult.write(f'{result} ')
@@ -25,7 +22,7 @@ def run(matrix1_path: Path, matrix2_path: Path) -> bool:
             elif count >= 4:
                 fresult.write(f'{result}\n')
                 count = 0
-    return filecmp.cmp(result_path, 'data/sum_matrix/.expected', shallow=False)
+    return filecmp.cmp(result_path, 'data\sum_matrix\.expected', shallow=False)
 
 
 if __name__ == '__main__':
