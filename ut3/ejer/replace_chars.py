7dcdf1221c7b1909cbb86d10c3b85401b7ff11b6
alherdom <alejandrohd1@live.com>
2023-02-04 21:09:20 +0000

    Sync

--- ut3/ejer/replace_chars.py
+++ ut3/ejer/replace_chars.py
@@ -5,20 +5,20 @@ import filecmp
 from pathlib import Path
 
 
-def run(input_path: Path, replacements: str) -> bool: 
-    replacements = replacements.split('|')
-    with open(input_path, encoding="utf8") as finput:    
-        news = finput.read()
-        
-        for old_char, new_char in zip(old_chars,new_chars):
-            news = news.replace(old_char,new_char)
-   
-        
-    output_path = 'data/replace_chars/r_noticia.txt'
-    with open(output_path, 'w',encoding="utf8") as foutput:
-        foutput.write(news)
-    return filecmp.cmp(output_path, 'data/replace_chars/.expected', shallow=False)
+def run(input_path: Path, replacements: str) -> bool:
+    chars = replacements.split("|")
+    with open(input_path) as f1:
+        noticia = f1.read()
+        for old_char, new_char in chars:
+            noticia = noticia.replace(old_char,new_char)
+    output_path = "data/replace_chars/r_noticia.txt"
+    with open(output_path, "w") as freplaced:
+        freplaced.write(noticia)
 
 
-if __name__ == '__main__':
-    run('data/replace_chars/noticia.txt', '�a|�e|�i|�o|�u')
+
+    return filecmp.cmp(output_path, "data/replace_chars/.expected", shallow=False)
+
+
+if __name__ == "__main__":
+    run("data/replace_chars/noticia.txt", "�a|�e|�i|�o|�u")
