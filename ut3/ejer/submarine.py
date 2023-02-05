ea9af8bcf7cbdeafc8155868a7e736d2c13a5cf1
alherdom <alejandrohd1@live.com>
2023-02-05 02:15:38 +0000

    Sync

--- ut3/ejer/submarine.py
+++ ut3/ejer/submarine.py
@@ -2,11 +2,23 @@
 # YELLOW SUBMARINE
 # ****************
 from pathlib import Path
-
-
+MIN_DEPTH = 0
+MAX_DEPTH = 600
+CONSUMPTION_RATE = 3
 def run(route_path: Path) -> tuple:
-    # TU CÓDIGO AQUÍ
-    distance = depth = fuel = 'output'
+    distance = depth = 0
+    with open(route_path) as f:
+        fuel = int(f.readline())
+        moves = [[int(h) for h in v.split(':')] for v in f.readline().strip().split(',')]
+        for x, y in moves:
+            distance += x
+            depth += y
+            fuel -= abs(x) * CONSUMPTION_RATE
+            if not fuel or depth > MAX_DEPTH or depth < MIN_DEPTH:
+                break
+      
+          
+      
 
     return distance, depth, fuel
 
