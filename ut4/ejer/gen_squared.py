254a11db5448f76651e8477315f806fc931aaf41
alherdom <alejandrohd1@live.com>
2023-02-14 12:58:53 +0000

    Sync

--- ut4/ejer/gen_squared.py
+++ ut4/ejer/gen_squared.py
@@ -1,7 +1,15 @@
-def gen_squared(n):
-    gen_list = []
-    for i in range(0,n+1):
-        number = i**2
-        gen_list.append(number)
-    return gen_list
-print(gen_squared(100))
+# def gen_squared(n):
+#     gen_list = []
+#     for i in range(0,n+1):
+#         number = i**2
+#         gen_list.append(number)
+#     return gen_list
+# print(gen_squared(100))
+
+def gen_squared(n=100):
+    for i in range(n):
+        yield i ** 2
+        
+for i in gen_squared():
+    print(i)
+    
