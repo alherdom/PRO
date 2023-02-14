3c9645e9673cab95f3f1ea77c64bc4931ac7bcbe
alherdom <alejandroh1@live.com>
2023-02-14 20:19:42 +0000

    Sync

---
+++ ut4/ejer/vending.py
@@ -0,0 +1,19 @@
+def order(choice,money):
+    ARTICLES = {1:'Coca-Cola',2:'Clipper',3:'Tirma',4:'Fanta',5:'Eidetesa'}
+    PRICES = {1:1.5,2:1.8,3:1.1,4:1.5,5:2.8}
+    STOCK = [20,30,20,20,10]
+    if PRICES[choice] == money:          
+        return(f'Dinero justo, aquí tiene su {ARTICLES[choice]} quedan') 
+    elif PRICES[choice] > money:
+        return(f'Dinero insuficiente, introduzca más dinero')
+    
+input_path = 'ut4/data/choices.dat'
+with open(input_path) as f:
+    for line in f:
+        elements = line.strip().split(",")
+        print(order(elements[0], elements[1]))
+        
+
+
+
+    
