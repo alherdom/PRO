6937c0f0e4ebcf262c703c2afee02d1cb730301f
alherdom <alejandroh1@live.com>
2023-02-15 19:25:39 +0000

    Sync

---
+++ ut4/ejer/consecutive_freqs.py
@@ -0,0 +1,29 @@
+# ************************************
+# FRECUENCIA DE ELEMENTOS CONSECUTIVOS
+# ************************************
+
+
+def cfreq(items,as_string=False):
+    freqs = []
+    elements = []
+    quantitys = []
+    i = 0
+    count = 0
+    for item in items:
+        if item not in elements:
+            elements.append(item)
+        if items[i] == item:
+            count += 1
+        if items.index(item) == len(items):
+            quantitys.append(count)
+            i += 1
+            count = 0
+    print(elements)
+    print(quantitys)
+    
+    if as_string:
+        return ''   
+    
+    return freqs                    
+    
+
