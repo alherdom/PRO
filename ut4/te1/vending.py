7a05d582d65db4b00aeb5ae7965d6aa41f6b03d7
alherdom <alejandrohd1@live.com>
2023-02-22 22:10:23 +0000

    Sync

--- ut4/te1/vending.py
+++ ut4/te1/vending.py
@@ -2,8 +2,10 @@ vending_path = 'ut4/te1/data/vending.dat'
 operations_path = 'ut4/te1/data/operations.dat'
 
 with open(vending_path, encoding="utf8") as f1, open(operations_path, encoding="utf8") as f2:
+    exchange = f1.readline().split()
     vending_list = f1.read().split()
     operations_list = f2.read().split()
+    print(exchange)
     print(vending_list)
     print(operations_list)
        
