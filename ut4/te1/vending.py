a6fc102452b785408fc44b6a55d031bd4b39536e
alherdom <alejandrohd1@live.com>
2023-02-22 22:51:56 +0000

    Sync

--- ut4/te1/vending.py
+++ ut4/te1/vending.py
@@ -2,12 +2,20 @@ vending_path = 'ut4/te1/data/vending.dat'
 operations_path = 'ut4/te1/data/operations.dat'
 
 with open(vending_path, encoding="utf8") as f1, open(operations_path, encoding="utf8") as f2:
-    exchange = f1.readline().split()
+    coins = f1.readline().split()
+    two_coin = coins[0]
+    one_coin = coins[1]
+    fifty_coin = coins[2]
     vending_list = f1.read().split()
     operations_list = f2.read().split()
-    print(exchange)
-    print(vending_list)
-    print(operations_list)
+    
+    for operation in operations_list:
+        if operation == 'O':
+            print('Its Order')
+        elif operation == 'R':
+            print('Its Restock')
+        elif operation == 'P':
+            print('Its Price')
        
 status_path = 'ut4/te1/data/status.dat'     
 with open(status_path, 'w', encoding="utf8") as f3:
