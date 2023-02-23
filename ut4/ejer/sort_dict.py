8165dd4e47ef51714f34142dc4d48a71100c0cf5
alherdom <alejandrohd1@live.com>
2023-02-23 23:13:18 +0000

    Sync

--- ut4/ejer/sort_dict.py
+++ ut4/ejer/sort_dict.py
@@ -6,7 +6,7 @@
 def run(unsorted_items: dict) -> list:
     sorted_items = []
     for item in unsorted_items.items():
-        sorted_items.append(item)
+         sorted_items.append(item)
     sorted_items = sorted(sorted_items, key=lambda t: t[1])
     return sorted_items
 if __name__ == '__main__':
