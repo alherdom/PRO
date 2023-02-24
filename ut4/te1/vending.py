9a7abf121f4713286cfa1e63eff4ac2679b8ed27
alherdom <alejandroh1@live.com>
2023-02-24 17:14:43 +0000

    Sync

--- ut4/te1/vending.py
+++ ut4/te1/vending.py
@@ -1,17 +1,37 @@
-vending_path = 'ut4/te1/data/vending.dat'
-operations_path = 'ut4/te1/data/operations.dat'
+VENDING_PATH = 'ut4/te1/data/vending.dat'
+OPERATION_PATH = 'ut4/te1/data/operations.dat'
 
-with open(vending_path, encoding="utf8") as f1, open(operations_path, encoding="utf8") as f2:
+with open(VENDING_PATH, encoding="utf8") as f1, open(OPERATION_PATH, encoding="utf8") as f2:
     coins = f1.readline().split()
     two_coin = coins[0]
     one_coin = coins[1]
     fifty_coin = coins[2]
-    vending_list = f1.read().split()
-    operations_list = f2.read().split()
+    vending_dict={}
+    operation_list = []
+    for line in f1:
+        line = line.strip().split()
+        vending_dict[line[0]]=line[1:]
     
-    for operation in operations_list:
+    for line in f2:
+        line = line.strip().split()
+        operation_list.append(line)
+
+    print(operation_list)
+    print(vending_dict)
+        
+        
+        
+        
+
+    
+    # def order(product,amount,first_coin,second_coin,third_coin):
+        
+        
+    
+    
+    for operation in operation_list:
         if operation == 'O':
-            print('Its Order')
+            print('Hola')
         elif operation == 'R':
             print('Its Restock')
         elif operation == 'P':
