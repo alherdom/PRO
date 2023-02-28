a021a9d6ec6602dcaed6b7e04273b5a5b936a91b
alherdom <alejandroh1@live.com>
2023-02-28 20:19:56 +0000

    Sync

--- ut4/te1/vending.py
+++ ut4/te1/vending.py
@@ -1,70 +1,28 @@
-VENDING_PATH = 'ut4/te1/data/vending.dat'
-OPERATION_PATH = 'ut4/te1/data/operations.dat'
+# ******************
+# MÁQUINA DE VENDING
+# ******************
+import filecmp
+from pathlib import Path
+OPERATION_PATH = 'data/vending/operations.dat'
+STATUS_PATH = 'data/vending/status.dat'
 
-with open(VENDING_PATH, encoding="utf8") as f1, open(OPERATION_PATH, encoding="utf8") as f2:
-    coins = f1.readline().split()
-    coins = [int(coin) for coin in coins]
-    products_dict={}
-    operations_list = []
-    
-    for line in f1:
-        line = line.strip().split()
-        products_dict[line[0]]=line[1:]
-    
-    for line in f2:
-        line = line.strip().split()
-        operations_list.append(line)
-
-
-for operation in operations_list:
-    match operation[0]:
-        case "O":
-            order_total_coins = [int(coin_amount) for coin_amount in operation[3:]]
-            calc_price = [int(operation[3]) * 2, int(operation[4]), int(operation[5])* 0.50]
-            product = operation[1]
-            requested_amount = operation[2]
-            stock,value = products_dict[product]
-            products_dict[product][0] = int(stock) - int(requested_amount)
-            # Esto seria una función
-            
-            coins = [value1 + value2 for value1, value2 in zip(order_total_coins,coins)]
-            
-            # Devolución de dinero
-            
-            total_paid = sum(calc_price)
-            total_price = float(value) * int(requested_amount)
-            if total_paid > total_price:
-                change = total_paid - total_price
-                types_of_coins = [2,1,0.50]
-                list_change = []
-                for coin in types_of_coins: 
-                    if change % coin == 0: 
-                        requested_amount = change/coin
-                        change -= requested_amount*coin
-                        list_change.append(int(requested_amount))
-                    else: 
-                        list_change.append(0)
-            
-            coins = [value1 - value2 for value1,value2 in zip(coins,list_change)]
-
+def run(operations_path: Path) -> bool:
+    operations = []
+    with open(OPERATION_PATH, encoding="utf8") as f1:
+        for line in f1:
+            line = line.strip().split()
+            operations.append(line)
+        for operation in operations:
+            match operation[0]:
                 
-                
-        # case "R":
-        #     print ("Se repone")
-        # case "P":
-        #     print ("Se cambia el precio")
-        # case "C":
-        #     print ("Se introduce dinero")
-                
-        
-        
-        
-
     
-    # def order(product,amount,first_coin,second_coin,third_coin):
-        
-        
-status_path = 'ut4/te1/data/status.dat'     
-with open(status_path, 'w', encoding="utf8") as f3:
-    f3.write(f'Hola')
+    
+    # ESCRITURA SALIDA "STATUS"
+    with open(STATUS_PATH, 'w', encoding="utf8") as f3:
+        f3.write(f'Hola')
 
+    return filecmp.cmp(STATUS_PATH, 'data/vending/.expected', shallow=False)
+
+
+if __name__ == '__main__':
+    run('data/vending/operations.dat')
