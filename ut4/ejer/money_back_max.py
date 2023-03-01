0d94e86fc16d41a3d296824e92b6e74f26e9eb8c
alherdom <alherdom@outlook.com>
2023-03-01 02:35:38 +0000

    Sync

--- ut4/ejer/money_back_max.py
+++ ut4/ejer/money_back_max.py
@@ -4,9 +4,15 @@
 
 
 def run(to_give_back: float, available_currencies: dict) -> dict:
-    # TU CÓDIGO AQUÍ
-    money_back = 'output'
-
+    money_back = {}
+    available_currencies = dict(sorted(available_currencies.items(), key=lambda item:item[0],reverse=True)) 
+    for currency, amount in available_currencies.items():
+        while currency <= to_give_back:
+            quot, rem = divmod(to_give_back, currency)             
+            money_back[currency] = quot
+            to_give_back -= quot * currency
+    if to_give_back != 0:
+        money_back = None
     return money_back
 
 
