1f50eaec000d4b7441c2c20bcc1278750f3367a3
alherdom <alherdom@outlook.com>
2023-03-01 02:45:53 +0000

    Sync

--- ut4/ejer/money_back.py
+++ ut4/ejer/money_back.py
@@ -9,12 +9,12 @@ def run(to_give_back: float, available_currencies: list) -> dict:
     for currency in available_currencies:
         while currency <= to_give_back:
             quot, rem = divmod(to_give_back, currency)
-            to_give_back -= quot * currency             
+            to_give_back -= quot * currency
             money_back[currency] = quot
     if to_give_back != 0:
         money_back = None
     return money_back
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run(20, [5, 2, 1])
--- ut4/ejer/money_back_max.py
+++ ut4/ejer/money_back_max.py
@@ -5,10 +5,12 @@
 
 def run(to_give_back: float, available_currencies: dict) -> dict:
     money_back = {}
-    available_currencies = dict(sorted(available_currencies.items(), key=lambda item:item[0],reverse=True)) 
+    available_currencies = dict(
+        sorted(available_currencies.items(), key=lambda item: item[0], reverse=True)
+    )
     for currency, amount in available_currencies.items():
         while currency <= to_give_back:
-            quot, rem = divmod(to_give_back, currency)        
+            quot, rem = divmod(to_give_back, currency)
             to_give_back -= quot * currency
             money_back[currency] = quot
     if to_give_back != 0:
@@ -16,5 +18,5 @@ def run(to_give_back: float, available_currencies: dict) -> dict:
     return money_back
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run(20, {5: 3, 2: 7, 1: 3})
