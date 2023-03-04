f6d46302333bbd1907e8966d779f04877c0c85e0
alherdom <alherdom@outlook.com>
2023-03-04 13:00:59 +0000

    Sync

--- ut4/te1/alfa_vending.py
+++ ut4/te1/alfa_vending.py
@@ -1,31 +1,42 @@
-OPERATION_PATH = 'ut4//te1/data/vending/operations.dat'
+OPERATION_PATH = "ut4//te1/data/vending/operations.dat"
 with open(OPERATION_PATH) as f:
     operations_list = [line.strip().split() for line in f]
-    
-money = 0        
+
+money = 0
+elements = []
+prices = {}
 status = {}
 for operation in operations_list:
     match operation[0]:
-        case "R":
-            status[operation[1]] = [operation[2]] 
+        case "R":        
+            status[operation[1]] = int(operation[2])
         case "P":
-            for product, element in status.items():
-                status[product] = [element[0],operation[2]]
+            price = int(operation[2])
+            if operation[1] in status:
+                prices[operation[1]] = int(operation[2])
+            else:
+                print('E1 PRODUCT NOT FOUND')
+        case "O":
+            if operation[1] in status:
+                paid = int(operation[3])
+                order = int(operation[2])
+                product = operation[1]
+                price = prices[operation[1]]
+                print(price)
+                bill = order * price
         case "M":
             money += int(operation[1])
-        case "O":
-            order_stock = int(operation[2])
-            insert_money = int(operation[3])
-            # print(status.get(operation[1], '❌ E1 PRODUCT NOT FOUND'))
-            for product, element in status.items():
-                status[product] = [int(element[1])-order_stock,int(operation[2])-insert_money]
-                                 
-                      
-            
-STATUS_PATH = 'ut4/te1/data/vending/status.dat'     
-with open(STATUS_PATH, 'w') as f:
-    f.write(f'{money}\n')
-    for product, state in sorted(status.items()):
-        specs = ' '.join(str(i) for i in state) + "\n"
-        f.write(f'{product} {specs}')
 
+for stock, price in zip(status.values(), prices.values()):
+    elements.append([stock, price])
+
+for key, element in zip(status.keys(), elements):
+    status[key] = element
+
+
+STATUS_PATH = "ut4/te1/data/vending/status.dat"
+with open(STATUS_PATH, "w") as f:
+    f.write(f"{money}\n")
+    for product, stock_price in sorted(status.items()):
+           specs = " ".join(str(i) for i in stock_price) + "\n"
+           f.write(f"{product} {specs}")
