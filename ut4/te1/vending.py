ba8ed9831d12bfc64ce6d1b20f14697181691308
alherdom <alherdom@outlook.com>
2023-03-05 13:30:25 +0000

    Se insertan def

--- ut4/te1/vending.py
+++ ut4/te1/vending.py
@@ -3,28 +3,36 @@
 # ******************
 import filecmp
 from pathlib import Path
+
 OPERATION_PATH = "data/vending/operations.dat"
 STATUS_PATH = "data/vending/status.dat"
+
+
 def run(operations_path: Path) -> bool:
     money = 0
     elements = []
     prices = {}
     status = {}
-    f = open(OPERATION_PATH)                                    # LECTURA FICHERO DE ENTRADA
+    f = open(OPERATION_PATH)  # LECTURA FICHERO DE ENTRADA
     operations_list = [line.strip().split() for line in f]
-    def restocking(operation: list) -> dict:                    # FUNCIÓN DE REASBASTECIMIENTO DE PRODUCTO
+
+    def restocking(operation: list) -> dict:  # FUNCIÓN DE REASBASTECIMIENTO DE PRODUCTO
         if operation[1] in status:
             status[operation[1]] += int(operation[2])
         else:
             status[operation[1]] = int(operation[2])
         return status
-    def pricing(operation: list) -> dict:                       # FUNCIÓN PARA FIJAR PRECIOS
+
+    def pricing(operation: list) -> dict:  # FUNCIÓN PARA FIJAR PRECIOS
         if operation[1] in status:
-                    prices[operation[1]] = int(operation[2])
+            prices[operation[1]] = int(operation[2])
         else:
             print("E1 PRODUCT NOT FOUND")
         return prices
-    def ordering(operation: list, money: int) -> dict:          # FUNCIÓN PARA PEDIDOS EN LA MÁQUINA
+
+    def ordering(
+        operation: list, money: int
+    ) -> dict:  # FUNCIÓN PARA PEDIDOS EN LA MÁQUINA
         if operation[1] in status:
             if status[operation[1]] >= int(operation[2]):
                 if int(operation[3]) >= (int(operation[2]) * prices[operation[1]]):
@@ -37,15 +45,17 @@ def run(operations_path: Path) -> bool:
         else:
             print("E1 PRODUCT NOT FOUND")
         return status
-    def reloading(operation: list, money: int) -> int:          # FUNCIÓN PARA RECARGA DE DINERO
+
+    def reloading(operation: list, money: int) -> int:  # FUNCIÓN PARA RECARGA DE DINERO
         money += int(operation[1])
         return money
+
     for operation in operations_list:
         match operation[0]:
             case "R":
-                restocking (operation)
+                restocking(operation)
             case "P":
-                pricing (operation)
+                pricing(operation)
             case "O":
                 if operation[1] in status:
                     if status[operation[1]] >= int(operation[2]):
@@ -64,11 +74,13 @@ def run(operations_path: Path) -> bool:
             case "M":
                 money += int(operation[1])
                 # reloading(operation,money)
-    for stock, price in zip(status.values(), prices.values()):  # LISTO STOCK Y PRECIO PARA FORMAR STATUS
+    for stock, price in zip(
+        status.values(), prices.values()
+    ):  # LISTO STOCK Y PRECIO PARA FORMAR STATUS
         elements.append([stock, price])
-    for key, element in zip(status.keys(), elements):           # SE CREA EL DICT STATUS
+    for key, element in zip(status.keys(), elements):  # SE CREA EL DICT STATUS
         status[key] = element
-    with open(STATUS_PATH, "w") as f:                           # ESCRITURA FICHERO DE SALIDA
+    with open(STATUS_PATH, "w") as f:  # ESCRITURA FICHERO DE SALIDA
         f.write(f"{money}\n")
         for product, stock_price in sorted(status.items()):
             specs = " ".join(str(i) for i in stock_price) + "\n"
