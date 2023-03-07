3c04c55940990b46d803df1713bbdd7f06328af1
alherdom <alejandroh1@live.com>
2023-03-07 20:19:44 +0000

    Sync

--- ut4/ejer/name_len.py
+++ ut4/ejer/name_len.py
@@ -1,9 +1,8 @@
 names = ["ana", "pepa", "maria"]
 
-
 def sum_len_names(names: list) -> int:
     size = len(names[1])
-    return size
+    return sum_len_names(names)
 
 
-sum_len_names(names)
+print(sum_len_names(names))
