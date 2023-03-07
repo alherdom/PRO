b2323034deb9e5b55b6acc558350c5193748c35d
alherdom <alejandrohd1@live.com>
2023-03-07 22:42:16 +0000

    Sync

--- ut4/ejer/magic_square.py
+++ ut4/ejer/magic_square.py
@@ -7,8 +7,8 @@ def is_magic_square(values: list) -> bool:
     if len(values) > 0:
         for value in values:
             sum_rows = [sum(value) for value in values]
-            avg_row = sum(sum_rows) / len(sum_rows)
-            if avg_row != sum(values[0]):
+            avg_rows = sum(sum_rows) / len(sum_rows)
+            if avg_rows != sum(values[0]):
                 return False
     return True
         
