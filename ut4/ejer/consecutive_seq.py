04faff1666d6ef6bb61065c4781edfd8a1d2ce5f
alherdom <alejandroh1@live.com>
2023-03-07 20:17:57 +0000

    Sync

--- ut4/ejer/consecutive_seq.py
+++ ut4/ejer/consecutive_seq.py
@@ -3,11 +3,19 @@
 # **************************************************
 
 
-def consecutive_seq(items: list, target_count: list) -> int:
-    for item in items:
-        times = items.count(item)
-        if times == target_count:
-            return item
-    return False
+# def consecutive_seq(items: list, target_count: list) -> int:
+#     for item in items:
+#         times = items.count(item)
+#         if times == target_count:
+#             return item
+#     return False
         
+def consecutive_seq(items: list, target_count: list, i: int) -> int:
+    i = 1
+    count = 0
+    if items[:i] == items[:i+1]:
+        count +=1
+    if count == target_count:
+        return items[:i]
+    return False
 
---
+++ ut4/ejer/name_len.py
@@ -0,0 +1,9 @@
+names = ["ana", "pepa", "maria"]
+
+
+def sum_len_names(names: list) -> int:
+    size = len(names[1])
+    return size
+
+
+sum_len_names(names)
