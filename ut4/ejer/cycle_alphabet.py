8de9e1bb85edf46e64bdf876f61d0821799ea9b3
alherdom <alejandrohd1@live.com>
2023-03-07 22:39:05 +0000

    Sync

--- ut4/ejer/cycle_alphabet.py
+++ ut4/ejer/cycle_alphabet.py
@@ -4,12 +4,11 @@
 
 
 def run(max_letters: int) -> str:
-    ABC = 'abcdefghijklmnopqrstuvwxyz'
-    if max_letters > 0:
-        times = max_letters // len(ABC)
-        limit = max_letters - (times * len(ABC))
-        return ABC * times + ABC[:limit]
-    return ''
+    ABC = "abcdefghijklmnopqrstuvwxyz"
+    times = max_letters // len(ABC)
+    limit = max_letters - (times * len(ABC))
+    return ABC * times + ABC[:limit]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run(43)
--- ut4/ejer/magic_square.py
+++ ut4/ejer/magic_square.py
@@ -3,6 +3,13 @@
 # ***************
 
 
-def is_magic_square():
-    # TU CÓDIGO AQUÍ
+def is_magic_square(values: list) -> bool:
+    if len(values) > 0:
+        for value in values:
+            sum_rows = [sum(value) for value in values]
+            avg_row = sum(sum_rows) / len(sum_rows)
+            if avg_row != sum(values[0]):
+                return False
+    return True
+        
 
