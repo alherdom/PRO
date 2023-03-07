d830dd3427d0235dc82d93df89820c7db58c2579
alherdom <alherdom@outlook.com>
2023-03-07 12:50:24 +0000

    Sync

--- ut4/ejer/palindrome.py
+++ ut4/ejer/palindrome.py
@@ -4,9 +4,9 @@
 
 
 def is_palindrome(text: str) -> bool:
+    text = text.replace(" ","").lower()
     for char1, char2 in zip(text, text[::-1]):
         if char1 == char2:
             return True    
-    return False
-    # TU CÓDIGO AQUÍ
+        return False
 
