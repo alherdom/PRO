761cec2c61e4b10c8f70444d38e5151bead22ba3
alherdom <alherdom@outlook.com>
2023-03-07 12:09:26 +0000

    Sync

--- ut4/ejer/num_in_interval.py
+++ ut4/ejer/num_in_interval.py
@@ -4,9 +4,7 @@
 
 
 def in_range(value: int, lower_limit: int, upper_limit: int) -> bool:
-    interval = []
-    for i in range(lower_limit, upper_limit+1):
-        interval.append(i)
+    interval = [i for i in range(lower_limit, upper_limit+1)]
     if value in interval:
         return True
     return False
