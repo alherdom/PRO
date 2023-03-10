58a0b17a556fc7baec774b10a467aff8cf7d34c0
alherdom <alherdom@outlook.com>
2023-03-10 12:52:19 +0000

    Sync

--- ut4/ejer/sum_nested.py
+++ ut4/ejer/sum_nested.py
@@ -3,6 +3,13 @@
 # ***********************
 
 
-def sum_nested():
-    # TU CÓDIGO AQUÍ
+def sum_nested(items: list, _sum:int=0) -> int:
+  
+        
+        if item.type == list:
+        
+        else:
+            _sum += int(item)
+    return _sum
+
 
