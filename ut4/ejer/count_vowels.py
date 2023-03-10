950a50bccff034de15cd93f4a6e8404d1a079db4
alherdom <alherdom@outlook.com>
2023-03-10 13:59:21 +0000

    Sync

--- ut4/ejer/count_vowels.py
+++ ut4/ejer/count_vowels.py
@@ -9,13 +9,14 @@
 #         if text[0] in VOWELS:
 #             num_vowels += 1
 #     else:
-#         return num_vowels    
+#         return num_vowels
 #     return count_vowels(text[1:],num_vowels)
+
 
 def count_vowels(text: str) -> int:
     VOWELS = "aeiou"
     if len(text) == 1:
         if text[0] in VOWELS:
             return 1
-        return 0    
+        return 0
     return count_vowels(text[0]) + count_vowels(text[1:])
