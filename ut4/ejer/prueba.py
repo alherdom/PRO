c24c294a2937aa4a6a06b4293a6d4d78772bfdad
alherdom <alherdom@outlook.com>
2023-03-11 18:36:33 +0000

    Sync

--- ut4/ejer/prueba.py
+++ ut4/ejer/prueba.py
@@ -1,10 +1,45 @@
-items = (1,2,2,1,3,2,4)
-value = 2
-ini = 0
-# count = 0
-# for i in items:
-#     if value == i:
-#         count +=1
+# ******************
+# MÁQUINA DE VENDING
+# ******************
+import filecmp
+from pathlib import Path
 
-count = [sum(i for i in items if i == value)/value]
-print(count)
+def read_file(operations_path: Path):
+    operations = [operation for operation in open(operations_path)]
+
+def restock(operation: list, stock: dict):
+    code = operation[1]
+    qty_restock = int(operation[2])
+    if code not in stock:
+        stock[code] = qty_restock
+    else:
+        stock[code] += qty_restock
+
+def change_price(operation: list, prices: dict):
+    code = operation[1]
+    new_price = int(operation[2])
+    prices[code] = new_price
+    
+def reload_money(operation:list, money: int):
+    money += int(operation[1])   
+
+def run(operations_path: Path) -> bool:
+    status_path = 'data/vending/status.dat'
+    stock = {}
+    prices = {}
+    money = 0
+    operations = read_file(operations_path)
+    for operation in operations:
+        match operation[0]:
+            case 'R':
+                restock(operation,stock)
+            case 'P':
+                change_price(operation,prices)
+            case 'M':
+                reload_money(operation,money)
+
+    return filecmp.cmp(status_path, 'data/vending/.expected', shallow=False)
+
+
+if __name__ == '__main__':
+    run('data/vending/operations.dat')
