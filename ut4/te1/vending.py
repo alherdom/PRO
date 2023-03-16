de69214435d4ced6bfd704f969d5798c84c51afc
alherdom <alejandroh1@live.com>
2023-03-16 18:10:36 +0000

    Sync

--- ut4/te1/vending.py
+++ ut4/te1/vending.py
@@ -4,10 +4,12 @@
 import filecmp
 from pathlib import Path
 
-def read_file(operations_path:str):
+
+def read_file(operations_path: str):
     return (line.strip().split() for line in open(operations_path))
 
-def restock(operation:list, stock:dict):
+
+def restock(operation: list, stock: dict):
     code = operation[1]
     qty_restock = int(operation[2])
     if code not in stock:
@@ -16,7 +18,8 @@ def restock(operation:list, stock:dict):
         stock[code] += qty_restock
     print(f'✅ {" ".join(operation)}')
 
-def change_price(operation:list, stock:dict, prices:dict):
+
+def change_price(operation: list, stock: dict, prices: dict):
     code = operation[1]
     if code in stock:
         new_price = int(operation[2])
@@ -25,7 +28,8 @@ def change_price(operation:list, stock:dict, prices:dict):
     else:
         print(f'❌ {" ".join(operation)} (E1: PRODUCT NOT FOUND)')
 
-def order(operation:list, stock:dict, prices:dict, money:int=0) -> int:
+
+def order(operation: list, stock: dict, prices: dict, money: int = 0) -> int:
     code = operation[1]
     qty_ordered = int(operation[2])
     user_money = int(operation[3])
@@ -43,20 +47,27 @@ def order(operation:list, stock:dict, prices:dict, money:int=0) -> int:
         print(f'❌ {" ".join(operation)} (E1: PRODUCT NOT FOUND)')
     return money
 
-def reload_money(operation:list, money:int=0) -> int:
+
+def reload_money(operation: list, money: int = 0) -> int:
     money += int(operation[1])
     print(f'✅ {" ".join(operation)}')
     return money
 
-def write_file(status_path:Path, money:int, stock:dict, prices:dict):
-     with open(status_path, "w") as f:
+
+def write_file(status_path: Path, money: int, stock: dict, prices: dict):
+    with open(status_path, "w") as f:
         f.write(f"{money}\n")
-        for code_stock, code_price in zip(sorted(stock.items()), sorted(prices.items())):
+        for code_stock, code_price in zip(
+            sorted(stock.items()), sorted(prices.items())
+        ):
             scode, stock = code_stock
             pcode, price = code_price
-            f.write(f'{scode} {stock} {price}\n')
+            f.write(f"{scode} {stock} {price}\n")
 
-def run(operations_path: Path, money:int=0, stock:dict={}, prices:dict={}) -> bool:
+
+def run(
+    operations_path: Path, money: int = 0, stock: dict = {}, prices: dict = {}
+) -> bool:
     status_path = "data/vending/status.dat"
     operations = read_file(operations_path)
     for operation in operations:
@@ -69,10 +80,11 @@ def run(operations_path: Path, money:int=0, stock:dict={}, prices:dict={}) -> bo
                 money += order(operation, stock, prices)
             case "M":
                 money += reload_money(operation)
-                
+
     write_file(status_path, money, stock, prices)
 
     return filecmp.cmp(status_path, "data/vending/.expected", shallow=False)
+
 
 if __name__ == "__main__":
     run("data/vending/operations.dat")
---
+++ ut5/ejer/prueba.py
@@ -0,0 +1,2 @@
+def prod(value1: int, value2: int) -> int:
+    result = value1 * value2
