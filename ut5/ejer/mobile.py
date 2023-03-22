068656c42be2448fce63ee9858849eaa6bc339dc
alherdom <alejandroh1@live.com>
2023-03-22 19:27:15 +0000

    Sync

--- ut5/ejer/mobile.py
+++ ut5/ejer/mobile.py
@@ -1,19 +1,39 @@
+OPERATION_CONSUMPTION_BATTERY = 1
+ON_CONSUMPTION_BATTERY = 5
+OFF_CONSUMPTION_BATTERY = 2
+
 class MobilePhone:
-    def __init__(self, manufacturer, screen_size, num_cores):
+    def __init__(self, manufacturer: str, screen_size: float, num_cores: int):
         self.manufacturer = manufacturer
         self.screen_size = screen_size
         self.num_cores = num_cores
-        self.status = 0
-        self.apps = []
-
-    def power_on(self):
-        self.status = 1
-
-    def power_off(self):
-        self.status = 0
-
-    def install_app(self, app):
-        self.apps.append(app)
-
-    def uninstall_app(self, app):
-        self.apps.remove(app)
+        self.apps = ["Whatsapp","YouTube","Twitter"]
+        self.status = False
+        self.battery = 100
+        
+    def switch_power(self):
+        if self.status:
+            self.battery -= OFF_CONSUMPTION_BATTERY
+        else:
+            self.battery -= ON_CONSUMPTION_BATTERY
+        self.status = not self.status
+    
+    def install_app(self, *apps: str ):
+        for app in apps:
+            if app not in self.apps:
+                self.apps.append(app)
+            else:
+                print("The app has been install")
+        
+    def uninstall_app(self, apps: list):
+        if apps in self.apps:
+            self.apps.remove(apps)
+        else:
+            print("The app has been unistall")
+            
+    def battery_recharge(self, battery):
+        to_charge = min(100, self.battery + battery)
+        self.battery = to_charge
+        
+iphone = MobilePhone("Apple",6.4,8)   
+iphone.
