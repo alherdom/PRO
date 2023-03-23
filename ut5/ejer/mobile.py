2bdeeedc012f2e7e80d6e2bff7a3da84538000e1
alherdom <alejandroh1@live.com>
2023-03-23 18:30:08 +0000

    Sync

--- ut5/ejer/mobile.py
+++ ut5/ejer/mobile.py
@@ -2,39 +2,43 @@ OPERATION_CONSUMPTION_BATTERY = 1
 ON_CONSUMPTION_BATTERY = 5
 OFF_CONSUMPTION_BATTERY = 2
 
+
 class MobilePhone:
-    def __init__(self, manufacturer: str, screen_size: float, num_cores: int, battery: int):
+    def __init__(
+        self, manufacturer: str, screen_size: float, num_cores: int, battery: int
+    ):
         self.manufacturer = manufacturer
         self.screen_size = screen_size
         self.num_cores = num_cores
-        self.apps = ["Whatsapp","YouTube","Twitter"]
+        self.apps = ["Whatsapp", "YouTube", "Twitter"]
         self.status = False
         self.battery = battery
-        
+
     def switch_power(self):
         if self.status:
             self.battery -= OFF_CONSUMPTION_BATTERY
         else:
             self.battery -= ON_CONSUMPTION_BATTERY
         self.status = not self.status
-    
-    def install_app(self, *apps: str ):
+
+    def install_app(self, *apps: str):
         for app in apps:
             if app not in self.apps:
                 self.apps.append(app)
             else:
                 print("The app has been install")
-        
+
     def uninstall_app(self, apps: list):
         if apps in self.apps:
             self.apps.remove(apps)
         else:
             print("The app has been unistall")
-            
+
     def battery_recharge(self, battery):
-        
+
         to_charge = min(100, self.battery + battery)
         self.battery = to_charge
-        
-iphone = MobilePhone("Apple",6.4,8,3000)   
+
+
+iphone = MobilePhone("Apple", 6.4, 8, 3000)
 iphone.install_app("Tiktok")
