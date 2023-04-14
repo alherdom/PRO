f506a86cac83cb5a8bc844d30a46f8f534c55d3f
alherdom <alejandroh1@live.com>
2023-04-14 15:22:09 +0100

    Sync

--- ut5/ejer/date.py
+++ ut5/ejer/date.py
@@ -1,16 +1,16 @@
 MONTHS = {
-    1: "enero",
-    2: "febrero",
-    3: "marzo",
-    4: "abril",
-    5: "mayo",
-    6: "junio",
-    7: "julio",
-    8: "agosto",
-    9: "septiembre",
-    10: "octubre",
-    11: "noviembre",
-    12: "diciembre",
+    1: ("enero", 31),
+    2: ("febrero", 28),
+    3: ("marzo", 31),
+    4: ("abril", 30),
+    5: ("mayo", 31),
+    6: ("junio", 30),
+    7: ("julio", 31),
+    8: ("agosto", 31),
+    9: ("septiembre", 30),
+    10: ("octubre", 31),
+    11: ("noviembre", 30),
+    12: ("diciembre", 31),
 }
 WEEKDAYS = {
     0: "domingo",
@@ -34,6 +34,7 @@ class Date:
         self.month = month
         self.year = year
         self.leap_year = False
+        
 
     def is_leap_year(self) -> bool:
         if self.year % 4 == 0 and self.year % 100 != 0:
