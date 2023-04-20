a1c4d4ba1a2a72490a66472436e09ea466a208af
alherdom <alejandrohd1@live.com>
2023-04-20 20:55:00 +0100

    Sync

--- ut5/ejer/file_inheritance.py
+++ ut5/ejer/file_inheritance.py
@@ -1,1 +1,48 @@
-...
+class File:
+    def __init__(self, path: str):
+        self.path = path
+        self.contents = []
+    
+    def add_content(self, content: str) -> list[str]:
+        return self.contents.append(content) # type: ignore
+    
+    def show_contents(self) -> str:
+        return f'✓ Contents:{self.contents}'
+        
+    @property
+    def size(self) -> int:
+        return sum(len(content) for content in self.contents)
+    
+    @property 
+    def info(self) -> str:
+        return f'✓ Path: {self.path} [size={self.size}B]'
+        
+class MediaFile(File):
+    def __init__(self, path: str, codec: str, geoloc: tuple, duration: int):
+        super().__init__(path)
+        self.codec = codec
+        self.geoloc = geoloc
+        self.duration = duration
+    
+    @property
+    def info(self):
+        file_info = super().info
+        return f'{file_info}\n✓ Codec: {self.duration}\n✓ Geolocation:{self.geoloc}\n✓ Duration: {self.duration}'
+    
+class VideoFile(MediaFile):
+    def __init__(self, path: str, codec: str, geoloc: tuple, duration: int, dimensions: tuple):
+        super().__init__(path, codec, geoloc, duration)
+        self.dimensions = dimensions
+    
+    @property
+    def info(self):
+        media_info = super().info
+        return f'{media_info}\n✓ Dimensions:{self.dimensions}'
+    
+vanrossum = VideoFile('/home/python/vanrossum.mp4', 'h264', (23.5454, 31.4343), 487, (1920, 1080))
+print(vanrossum.info)
+vanrossum.add_content('audio/ogg')
+vanrossum.add_content('video/webm')
+vanrossum.add_content('img/jpg')
+print(f'✓ Size: {vanrossum.size}')
+print(vanrossum.show_contents())
