c9517cee75bc3b001b9956384295b691bad4e656
alherdom <alejandrohd1@live.com>
2023-04-25 22:53:04 +0100

    Sync

--- ut5/ejer/fibonacci_iterable.py
+++ ut5/ejer/fibonacci_iterable.py
@@ -2,31 +2,28 @@
 # FIBONACCI ITERABLE
 # ******************
 
-# num_ini = 0
-# num_ulti = 1
-# num_result = 0
-# for _ in range(98):
-#     num_result = num_ini + num_ulti
-#     num_ini = num_ulti
-#     num_ulti = num_result
-
-
 class Fibonacci:
         def __init__(self, n: int):
             self.limit = n
             self.pointer = 0
-            self.values = []
+            self.values = [0]
+            initial_value = -1
+            last_value = 1
+            next_value = 0
+            while len(self.values) < self.limit + 1:
+                next_value = initial_value + last_value
+                initial_value = last_value
+                last_value = next_value
+                self.values.append(next_value)
         
         def __iter__(self):
             return self
 
         def __next__(self):
-            
             if self.pointer >= self.limit:
                 raise StopIteration
-            
             self.pointer += 1
-            
-            return ...
+            return self.values[self.pointer]
 
-# def run():
+def run(n):
+    return list(Fibonacci(n))
