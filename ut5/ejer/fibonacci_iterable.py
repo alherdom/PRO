6e3b9467de6d106815999a86c5ab8bdaff356599
Alejandro Hernández <alherdom@outlook.com>
2023-04-27 13:03:06 +0100

    Sync

--- ut5/ejer/fibonacci_iterable.py
+++ ut5/ejer/fibonacci_iterable.py
@@ -6,18 +6,11 @@ class Fibonacci:
         def __init__(self, n: int):
             self.limit = n
             self.pointer = 0
-            self.values = [0]
-            initial_value = -1
-            last_value = 1
-            next_value = 0
-            while len(self.values) <= self.limit:
-                next_value = initial_value + last_value
-                initial_value = last_value
-                last_value = next_value
-                self.values.append(next_value)
+            self.first_value = -1
+            self.second_value = 1
                 
         def __str__(self):
-            return f'Fibo list is: {self.values[1:]}'
+            return f'Fibo list is: {self.next_value}'
         
         def __iter__(self):
             return self
@@ -25,15 +18,18 @@ class Fibonacci:
         def __next__(self):
             if self.pointer >= self.limit:
                 raise StopIteration
+            self.next_value = self.first_value + self.second_value
+            self.first_value = self.second_value
+            self.second_value = self.next_value
             self.pointer += 1
-            return self.values[self.pointer]
+            return self.next_value
 
-fibo1 = Fibonacci(10)
-print(fibo1)
-print(next(fibo1))
-print(next(fibo1))
-print(next(fibo1))
-print(next(fibo1))
+# fibo1 = Fibonacci(10)
+# print(fibo1)
+# print(next(fibo1))
+# print(next(fibo1))
+# print(next(fibo1))
+# print(next(fibo1))
 
 def run(n):
     return list(Fibonacci(n))
