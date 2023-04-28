1f2caa4ca5a9124fce2be98f1489599e7b2dca98
alherdom <alejandroh1@live.com>
2023-04-28 17:08:15 +0100

    Sync

--- ut5/ejer/dna.py
+++ ut5/ejer/dna.py
@@ -5,7 +5,6 @@ class DNA:
     CYTOSINE = "C"
     GUANINE = "G"
     THYMINE = "T"
-    ACGT = [ADENINE,CYTOSINE,GUANINE,THYMINE]
 
     def __init__(self, sequence: str):  
         self.sequence = sequence
@@ -32,15 +31,11 @@ class DNA:
     def __len__(self) -> int:
         return len(self.sequence)
 
-    @property
-    def length(self) -> int:
-        return len(self.sequence)
-
     def __add__(self, other) -> DNA:
         new_sequence = ""
         for char1, char2 in zip(self.sequence, other.sequence):
             new_sequence += max(char1, char2)
-        new_sequence += self.sequence[other.length:] if self.length > other.length else other.sequence[self.length:]
+        new_sequence += self.sequence[len(other):] if len(self) > len(other) else other.sequence[len(self):]
         return DNA(new_sequence)
 
     def __mul__(self, other) -> DNA:
@@ -53,7 +48,7 @@ class DNA:
         return self.sequence[index]
 
     def __setitem__(self, index: int, char: str):
-        new_char = char if char in self.ACGT else self.ADENINE
+        new_char = char if char in self.stats() else DNA.ADENINE
         self.sequence = self.sequence[:index] + new_char + self.sequence[index + 1:]
 
     @staticmethod
@@ -61,16 +56,17 @@ class DNA:
         return (qty_base / dna_size) * 100
 
     def stats(self) -> dict:
-        pct_adenine = DNA.calc_percent(self.adenines, self.length)
-        pct_cytosine = DNA.calc_percent(self.cytosines, self.length)
-        pct_guanine = DNA.calc_percent(self.guanines, self.length)
-        pct_thymine = DNA.calc_percent(self.thymines, self.length)
-        return {self.ADENINE:pct_adenine,self.CYTOSINE:pct_cytosine,self.GUANINE:pct_guanine,self.THYMINE:pct_thymine}    
+        dna_size = len(self)
+        pct_adenine = DNA.calc_percent(self.adenines, dna_size)
+        pct_cytosine = DNA.calc_percent(self.cytosines, dna_size)
+        pct_guanine = DNA.calc_percent(self.guanines, dna_size)
+        pct_thymine = DNA.calc_percent(self.thymines, dna_size)
+        return {DNA.ADENINE:pct_adenine,DNA.CYTOSINE:pct_cytosine,DNA.GUANINE:pct_guanine,DNA.THYMINE:pct_thymine}    
 
     @classmethod
     def build_from_file(cls, path: str) -> DNA:
         return DNA(open(path).read())
 
-    def dump_to_file(self, path):    
+    def dump_to_file(self, path: str):    
         with open(path, "w") as f:
             f.write(self.sequence)
