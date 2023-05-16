60e40c6199b3733b879c324d7b4558245a558be6
alherdom <alherdom@outlook.com>
2023-05-16 15:42:22 +0100

    Sync

--- ut5/ejer/prueba3.py
+++ ut5/ejer/prueba3.py
@@ -5,7 +5,7 @@ bits_in_ip = bits_in_octet * 4
 octets_ones = '.'.join(['1' * 8] * 4)
 octets_ceros = '.'.join(['0' * 8] * 4) 
 
-binary_ip = '.'.join([f'{int(octet):08b}' for octet in ip.split('.')])
+binary_ip = '.'.join(f'{int(octet):08b}' for octet in ip.split('.'))
 nsh_index = int(cidr) + (int(cidr) // 8)
 network_part = binary_ip[:nsh_index]
 host_part = binary_ip[nsh_index:]
@@ -23,16 +23,36 @@ def binary_2_decimal(octets: str):
     return '.'.join(str(int(octet,2)) for octet in octets.split('.'))
     
 def decimal_2_binary(octets: str):
-    return '.'.join([f'{int(octet):08b}' for octet in octets.split('.')])
+    return '.'.join(f'{int(octet):08b}' for octet in octets.split('.'))
 
 def get_number_hosts(cidr: int):
         if cidr in (31, 32):
             return 2 ** (32 - cidr)
         return 2 ** (32 - cidr) - 2
+
+def from_mask_to_cidr(mask: str | list):
+    if isinstance(mask, str):
+        binary_mask = decimal_2_binary(mask)
+    
+
+def highest_cidr(ip1: str, ip2: str):
+    common_ip = ""
+    binary_ip1 = decimal_2_binary(ip1)
+    binary_ip2 = decimal_2_binary(ip2)
+    for bit1, bit2 in zip(binary_ip1, binary_ip2):
+        if bit1 != bit2:
+            return len(common_ip.replace('.',''))
+        common_ip += bit1
         
-print(binary_ip)
-print(cidr)
-print(ip)
-print(decimal_id_red)
-print(decimal_broadcast)
-print(decimal_mask)
+                    
+# print(binary_ip)
+# print(cidr)
+# print(ip)
+# print(decimal_id_red)
+# print(binary_ip)
+# print(decimal_broadcast)
+# print(binary_broadcast)
+# print(decimal_mask)
+print(decimal_2_binary('172.140.35.10'))
+print(decimal_2_binary('172.140.35.63'))
+print(highest_cidr('172.140.35.10','172.140.35.63'))
