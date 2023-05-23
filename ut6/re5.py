eb29b7b7a0675f9e7fd15b7586953f5e39c0e147
alherdom <alejandroh1@live.com>
2023-05-24 00:59:24 +0100

    Sync

--- ut6/re5.py
+++ ut6/re5.py
@@ -3,3 +3,19 @@ import re
 # entre números enteros positivos. Las operación puede ser suma, resta,
 # multiplicación o división, y puede haber espacios (o no) entre los operandos
 # y el operador.
+operations = ('4  +2', '3-  2','4 * 2','7/2' )
+regexp = r'(\d)\s*?([+\-*\/])\s*?(\d)'
+for operation in operations:
+    m = re.search(regexp, operation)
+    first_num = int(m[1])
+    operator = m[2]
+    second_num = int(m[3])
+    match operator:
+        case '+':
+            print(first_num + second_num)
+        case '-':
+            print(first_num - second_num)
+        case '*':
+            print(first_num * second_num)
+        case '/':
+            print(first_num / second_num)
