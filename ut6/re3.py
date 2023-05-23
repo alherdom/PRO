ea8921bbbc591ec44afc24904d1007baac4cb5a2
alherdom <alejandroh1@live.com>
2023-05-23 20:19:42 +0100

    Sync

--- ut6/re3.py
+++ ut6/re3.py
@@ -2,10 +2,8 @@ import re
 # Escriba un programa en Python que indique si un determinado número es o no 
 # un flotante válido en Python.
 
-number1 = '02.00' 
-number2 = '02'
-number3 = '30.00.00'
-
-regex = r'\d+\.\d+'
-output = re.match(regex, number2)
-print(output)
+numbers = ('02.00', '02', '30.00.00', '3e0000', '3.0000_0000', '4.')
+regex = r'\d+\.?e?\d+_?\d+?'
+for number in numbers:
+    output = re.match(regex, number)
+    print(output)
