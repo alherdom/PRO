fe5f2a026fd856990af59a6fddfbe9a339bbd1b7
alherdom <alejandroh1@live.com>
2023-05-25 18:22:01 +0100

    Sync

--- ut6/todo.py
+++ ut6/todo.py
@@ -12,7 +12,9 @@ class Task:
     - con: para la conexión a la base de datos.
     - cur: para el cursor de manejo.'''
     con = sqlite3.connect(DB_PATH)
-    cur = con.cursor() 
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    
 
     def __init__(self, name: str, done: bool = False, id: int = -1):
         '''Crea los atributos homónimos a los parámetros'''
@@ -28,7 +30,6 @@ class Task:
         Task.cur.execute(sql,(self.name, self.done))
         Task.con.commit()
         self.id = Task.cur.lastrowid
-    
     
     def update(self):
         '''Actualiza la tarea (nombre y estado) en la base de datos'''
@@ -54,21 +55,20 @@ class Task:
     @classmethod
     def from_db_row(cls, row: sqlite3.Row) -> Task:
         '''Construye una nueva tarea a partir de una fila de consulta devuelta por execute()'''
+        # return Task(row[1], row[2], row[0])
         return Task(row['name'], row['done'], row['id'])
+
 
     @classmethod
     def get(cls, task_id: int) -> Task:
         '''Devuelve un objeto Task desde la consulta a la base de datos'''
         res = Task.cur.execute(f'SELECT * FROM tasks WHERE id={task_id}')
-        id, name, done = res.fetchone()
-        return Task(name, done, id)
-
+        return cls.from_db_row(res.fetchone())
 
 class ToDo:
     '''Crear atributos de clase:
     - con: para la conexión a la base de datos.
     - cur: para el cursor de manejo.'''
-
     con = sqlite3.connect(DB_PATH)
     con.row_factory = sqlite3.Row
     cur = con.cursor()
@@ -80,7 +80,8 @@ class ToDo:
                     name CHAR,
                     done INTEGER
                 )'''
-        
+        Task.cur.execute(sql)
+        Task.con.commit()
 
     def get_tasks(self, *, done: int = -1):
         '''Devuelve todas las tareas como objetos de tipo Task consultando la BBDD.
@@ -92,16 +93,12 @@ class ToDo:
 
     def add_task(self, name: str):
         '''Añade la tarea con nombre "name"'''
-        Task.con.cursor().execute('INSERT INTO tasks(name) VALUES(?)',(name,))
-        Task.con.commit()
-
-
+        Task(name).save()
+        
     def complete_task(self, task_id: int):
         '''Marca la tarea con identificador "task_id" como completada'''
-        Task.cur.execute(f'UPDATE tasks SET done={True} WHERE id={task_id}')
-        Task.con.commit()
+        Task.get(task_id).check()
 
     def reopen_task(self, task_id: int):
         '''Marca la tarea con identificador "task_id" como pendiente'''
-        Task.cur.execute(f'UPDATE tasks SET done={False} WHERE id={task_id}')
-        Task.con.commit()
+        Task.get(task_id).uncheck()
