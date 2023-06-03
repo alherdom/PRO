1292809796c6d820f5da8bb2d63af28124572bc4
alherdom <alherdom@outlook.com>
2023-06-03 13:03:49 +0100

    Sync

--- ut5/te2/src/game.py
+++ ut5/te2/src/game.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
-from deck import Deck
-from players import Player
-from dealer import Dealer
-from hand import Hand
+from cards import Card, Deck, Hand
+from roles import Dealer, Player
 
 class Game:
     def __init__(self, number_players: int):
@@ -13,6 +11,15 @@ class Game:
     
     def start_game(self):
         Dealer.give_cards(self)
+    
+    def __str__(self):
+        print(self)
+    
+    def show_players(self):
+        print(self.players)
+    
+    def get_winner(self):
+        pass
         
 
 # - Datos:
@@ -25,3 +32,9 @@ class Game:
 #   - Crear el dealer ✔
 #   - Comenzar la partida (repartir cartas, buscar mejor combinación)
 #   - Finalizar la partida (mostrar el ganador y su mano)
+
+new_game = Game(3)
+
+new_game.start_game()
+
+print(new_game.show_players())
